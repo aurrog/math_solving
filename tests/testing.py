44cc4ebd596676d0266eb68eed5af9633828fc8c
aurrog <gnusovme@gmail.com>
2024-03-19 22:53:24 +0300

    Start project

--- tests/testing.py
+++ tests/testing.py
@@ -1,22 +1,31 @@
 import cv2
+import time
 
 
-ip = ''  # put your ip address here
+ip = '192.168.1.100'  # put your ip address here
 port = '4747'
 video_capture = cv2.VideoCapture(f"http://{ip}:{port}/video")
 
+is_photo = False
 
 while True:
     ret, image = video_capture.read()
 
     image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-    # image = cv2.resize(image, (28, 28))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('Video', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        is_photo = True
+        break
 
 
 video_capture.release()
 cv2.destroyAllWindows()
+
+if is_photo:
+    cv2.imshow('photo', image)
+    time.sleep(2)
+    cv2.waitKey(0)
