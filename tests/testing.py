9f5dce4b155f17567e4f739cb16a177e746bab05
aurrog <gnusovme@gmail.com>
2024-03-25 22:17:46 +0300

    Start project

--- tests/testing.py
+++ tests/testing.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 
 
 ip = '192.168.1.100'  # put your ip address here
@@ -12,9 +13,21 @@ while True:
     ret, image = video_capture.read()
 
     image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Video', image)
+    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # cv2.imwrite(image_path, cleaned)
+    # cv2.imshow('image', cleaned)
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        symbol = image[y:y + h, x:x + w]
+
+    cv2.imshow('', contours)
+
+    # cv2.imshow('Video', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if cv2.waitKey(1) & 0xFF == ord('w'):
